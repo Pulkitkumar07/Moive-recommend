01e26dac202ce0633e6f1e358a915a4ac1d2f3a5
Pulkitkumar07 <pulkitchaudhary519@gmail.com>
2026-01-20 12:30:52 +0530

    fixed some issue

--- Server/app.js
+++ Server/app.js
@@ -1,22 +1,39 @@
 import express from "express";
 import cors from "cors";
 import MainRoutes from "./src/rotues/MainRoutes.js";
-import path from 'path'
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 
 app.use(express.json());
 
 
-app.use(cors({
-  origin: ["https://moive-recommend-1.onrender.com"] 
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://moive-recommend-1.onrender.com"],
+  })
+);
+
+
+app.use("/api", MainRoutes);
 
 
 app.use(express.static(path.join(__dirname, "../public")));
+
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
-app.use('/api', MainRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 export default app;
