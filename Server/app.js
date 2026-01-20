0c1adf577daa4c0f15554b555730d916f9ddd2ea
Pulkitkumar07 <pulkitchaudhary519@gmail.com>
2026-01-20 12:35:08 +0530

    ..

--- Server/app.js
+++ Server/app.js
@@ -4,6 +4,7 @@ import MainRoutes from "./src/rotues/MainRoutes.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -11,7 +12,6 @@ const app = express();
 
 
 app.use(express.json());
-
 
 app.use(
   cors({
